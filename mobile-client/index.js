0c45583d901298d9a101eb30461edd6a80dd1b75
Jalaj-01 <jalajgupta550@gmail.com>
2026-07-19 23:29:16 +0530

    fix: inline DOMException polyfill in index.js to prevent ES6 import hoisting issues

--- mobile-client/index.js
+++ mobile-client/index.js
@@ -1,5 +1,17 @@
-import './src/polyfills';
-import registerRootComponent from 'expo/src/launch/registerRootComponent';
-import App from './App';
+// Polyfill global DOMException to support modern third-party package APIs in Hermes
+if (typeof global.DOMException === 'undefined') {
+  class DOMException extends Error {
+    constructor(message, name) {
+      super(message);
+      this.name = name || 'DOMException';
+    }
+  }
+  global.DOMException = DOMException;
+}
+
+// Use require instead of import to prevent ES6 module hoisting
+// This ensures that the polyfill executes strictly before any other module is evaluated.
+const registerRootComponent = require('expo/src/launch/registerRootComponent').default;
+const App = require('./App').default;
 
 registerRootComponent(App);
--- mobile-client/src/polyfills.ts
+++
@@ -1,9 +0,0 @@
-// Polyfill global DOMException to support modern third-party package APIs in Hermes
-if (typeof global.DOMException === 'undefined') {
-  (global as any).DOMException = class DOMException extends Error {
-    constructor(message: string, name?: string) {
-      super(message);
-      this.name = name || 'DOMException';
-    }
-  };
-}
